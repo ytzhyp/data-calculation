49201080f4b240d2d04de4d65e642b069b29361a
ytzhyp <359614724@qq.com>
2019-03-27 11:59:54 +0800

    第一天（2019.3.27）
    01.Classical Binary Search
    Description
    Find any position of a target number in a sorted array. Return -1 if target does not exist.
    
    Example
    Example 1:
    
    Input: nums = [1,2,2,4,5,5], target = 2
    Output: 1 or 2
    
    Example 2:
    Input: nums = [1,2,2,4,5,5], target = 6
    Output: -1
    oj: https://www.lintcode.com/problem/classical-binary-search/description

--- 01.Classical Binary Search/Demo.java
+++ 01.Classical Binary Search/Demo.java
@@ -1,3 +1,17 @@
+    01.Classical Binary Search
+    Description
+    Find any position of a target number in a sorted array. Return -1 if target does not exist.
+
+    Example
+    Example 1:
+
+    Input: nums = [1,2,2,4,5,5], target = 2
+    Output: 1 or 2
+
+    Example 2:
+    Input: nums = [1,2,2,4,5,5], target = 6
+    Output: -1
+    oj: https://www.lintcode.com/problem/classical-binary-search/description
     /**
      * 01.Classical Binary Search
      * @param nums An int array sorted in ascending order
@@ -20,3 +34,4 @@
         }
         return -1;
     }
+
